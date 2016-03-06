f5627a25be05b554313290251f2d7c301996c69a
Tom Schindl <tom.schindl@bestsolution.at>
2016-03-06 10:23:14 -0500

    use @Component for theme

--- at.bestsolution.e4fx.app/src/at/bestsolution/e4fx/app/themes/DefaultTheme.java
+++ at.bestsolution.e4fx.app/src/at/bestsolution/e4fx/app/themes/DefaultTheme.java
@@ -1,9 +1,39 @@
 package at.bestsolution.e4fx.app.themes;
 
+import org.eclipse.fx.ui.services.theme.MultiURLStylesheet;
+import org.eclipse.fx.ui.services.theme.Stylesheet;
+import org.eclipse.fx.ui.services.theme.Theme;
 import org.eclipse.fx.ui.theme.AbstractTheme;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
+@Component(service=Theme.class)
 public class DefaultTheme extends AbstractTheme {
 	public DefaultTheme() {
 		super("theme.default", "Default theme", DefaultTheme.class.getClassLoader().getResource("css/default.css"));
+	}
+
+	@Reference(cardinality=ReferenceCardinality.MULTIPLE,policyOption=ReferencePolicyOption.GREEDY)
+	@Override
+	public void registerStylesheet(Stylesheet stylesheet) {
+		super.registerStylesheet(stylesheet);
+	}
+
+	@Override
+	public void unregisterStylesheet(Stylesheet stylesheet) {
+		super.unregisterStylesheet(stylesheet);
+	}
+
+	@Reference(cardinality=ReferenceCardinality.MULTIPLE,policyOption=ReferencePolicyOption.GREEDY)
+	@Override
+	public void registerMultiURLStylesheet(MultiURLStylesheet stylesheet) {
+		super.registerMultiURLStylesheet(stylesheet);
+	}
+
+	@Override
+	public void unregisterMultiURLStylesheet(MultiURLStylesheet stylesheet) {
+		super.unregisterMultiURLStylesheet(stylesheet);
 	}
 }
